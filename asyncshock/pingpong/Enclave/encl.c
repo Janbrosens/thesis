87ac7453ab5dfec371f04a8ac3aebdb86c8c344a
Jan Brosens <jan.brosens.db@gmail.com>
2025-02-22 13:17:23 +0000

    update ping pong

--- asyncshock/pingpong/Enclave/encl.c
+++ asyncshock/pingpong/Enclave/encl.c
@@ -8,23 +8,13 @@ sgx_thread_cond_t cond = SGX_THREAD_COND_INITIALIZER;
 int turn = 0;  // 0 for ping, 1 for pong
 
 void ecall_ping() {
-    sgx_thread_mutex_lock(&mutex);
-    while (turn != 0) {
-        sgx_thread_cond_wait(&cond, &mutex);
-    }
+    
     ocall_print("[SGX] Ping!\n");
-    turn = 1;  // Switch turn
-    sgx_thread_cond_signal(&cond);
-    sgx_thread_mutex_unlock(&mutex);
+    
 }
 
 void ecall_pong() {
-    sgx_thread_mutex_lock(&mutex);
-    while (turn != 1) {
-        sgx_thread_cond_wait(&cond, &mutex);
-    }
+    
     ocall_print("[SGX] Pong!\n");
-    turn = 0;  // Switch turn
-    sgx_thread_cond_signal(&cond);
-    sgx_thread_mutex_unlock(&mutex);
+    
 }
